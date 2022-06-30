5315f26c9efa33711b2f748df0548f5d0b162af4
Bhupendra sambare <bhupendrasam1404@gmail.com>
2022-07-01 00:15:58 +0530

    Added solution - LeetHub

---
+++ Trapping Rain Water - GFG/trapping-rain-water.java
@@ -0,0 +1,65 @@
+// { Driver Code Starts
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+
+class Array {
+
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+		while(t-->0){
+		  
+		    //size of array
+		    int n = Integer.parseInt(br.readLine().trim());
+		    int arr[] = new int[n];
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    
+		    //adding elements to the array
+		    for(int i=0; i<n; i++){
+		        arr[i] = Integer.parseInt(inputLine[i]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    
+		    //calling trappingWater() function
+		    System.out.println(obj.trappingWater(arr, n));
+		}
+	}
+}
+
+// } Driver Code Ends
+
+
+class Solution{
+    
+    static long trappingWater(int heights[], int n) { 
+         // maintain two pointers left and right, pointing to the leftmost and
+        // rightmost index of the input array
+        int left = 0, right = heights.length - 1;
+        long water = 0;
+ 
+        int maxLeft = heights[left];
+        int maxRight = heights[right];
+ 
+        while (left < right)
+        {
+            if (heights[left] <= heights[right])
+            {
+                left++;
+                maxLeft = Integer.max(maxLeft, heights[left]);
+                water += (maxLeft - heights[left]);
+            }
+            else {
+                right--;
+                maxRight = Integer.max(maxRight, heights[right]);
+                water += (maxRight - heights[right]);
+            }
+        }
+ 
+        return water;
+    } 
+}
+
+
