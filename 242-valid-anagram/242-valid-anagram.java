27b498b6ad37626ed6887503f8727dcd67cfc9b9
Bhupendra sambare <bhupendrasam1404@gmail.com>
2022-07-04 19:10:32 +0530

    Time: 5 ms (74.38%), Space: 42.8 MB (85.60%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+        
+        int[] check = new int[26];
+        
+        for(int i=0; i<s.length(); i++) {
+            check[s.charAt(i)-'a'] += 1;
+            check[t.charAt(i)-'a'] -= 1;
+        }
+        
+        for(int i=0; i<26; i++) {
+            if(check[i] != 0) return false;
+        }
+        
+        return true;
+    }
+}
